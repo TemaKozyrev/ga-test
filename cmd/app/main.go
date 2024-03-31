2e81f500803d6d598edcfea524d355aaeceb29fe
Artemiy Kozyrev <a.a.kozyrev@tinkoff.ru>
2024-03-31 13:44:27 +0300

    remove comment

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -13,20 +15,20 @@ func main() {
 	logger.SetLevel(log.InfoLevel)
 	logger.SetFormatter(&log.TextFormatter{})
 
-	//conn, err := pgx.Connect(context.Background(), "postgres://test:test@localhost:5432/test")
-	//if err != nil {
-	//	log.Error(err)
-	//}
-	//defer conn.Close(context.Background())
+	conn, err := pgx.Connect(context.Background(), "postgres://test:test@localhost:5432/test")
+	if err != nil {
+		log.Error(err)
+	}
+	defer conn.Close(context.Background())
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		//if err := conn.Ping(r.Context()); err != nil {
-		//	w.Write([]byte(err.Error()))
-		//} else {
+		if err := conn.Ping(r.Context()); err != nil {
+			w.Write([]byte(err.Error()))
+		} else {
 			w.Write([]byte("welcome"))
-		//}
+		}
 	})
 
 	http.ListenAndServe(":3000", r)
