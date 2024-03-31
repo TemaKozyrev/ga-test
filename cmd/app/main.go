af069801d70b312a0d40e2962be366cdb553fa95
Artemiy Kozyrev <a.a.kozyrev@tinkoff.ru>
2024-03-31 10:48:16 +0300

    add db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("postgres://test:test@localhost:5432/test"))
+	if err != nil {
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		if err := conn.Ping(r.Context()); err != nil {
+			w.Write([]byte(err.Error()))
+		} else {
+			w.Write([]byte("welcome"))
+		}
 	})
+
 	http.ListenAndServe(":3000", r)
 }
